37e85f40c95af4ec950a25a20441680336f9e6be
Christy <christypjubi@gmail.com>
2024-04-12 20:47:03 +0530

    initial commit

---
+++ react-project/codepen-clone/src/components/Home.jsx
@@ -0,0 +1,18 @@
+
+
+
+import Header from "./Header";
+import Code from "./code";
+import Result from "./Result";
+
+const Home = () => {
+    return(
+        <>
+    <Header/>
+    <Code/>
+    <Result/>
+    </>
+    )
+}
+
+export default Home;
